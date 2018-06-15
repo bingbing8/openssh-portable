33b4bdef41af794e35f3b80564ec7d5159b5c5d6
Yanbing Wang <yawang@microsoft.com>
2018-06-15 13:34:35 -0700

    1. Update debug message 2. Replace LsaNtStatusToWinError (non-onecore compatible) with RtlNtStatusToDosError

--- contrib/win32/win32compat/win32_usertoken_utils.c
+++ contrib/win32/win32compat/win32_usertoken_utils.c
@@ -460,8 +460,11 @@ AddSidMappingToLsa(PUNICODE_STRING domain_name,
 			error("LsaManageSidNameMapping failed with ntstatus: %d \n", status);
 	}
 
-	if (p_output)
-		LsaFreeMemory(p_output);
+	if (p_output) {
+		status = LsaFreeMemory(p_output);
+		if (status != STATUS_SUCCESS)
+			debug3("LsaFreeMemory failed with ntstatus: %d \n", status);
+	}
 
 	return ret;
 }
@@ -485,11 +488,12 @@ int RemoveVirtualAccountLSAMapping(PUNICODE_STRING domain_name,
 		&p_output);
 	if (status != STATUS_SUCCESS)
 		ret = -1;
-
-	/* TODO - Free p_output */
-	/*if (p_output)
-		LsaFreeMemory(p_output);*/
-
+		
+	if (p_output) {
+		status = LsaFreeMemory(p_output);
+		if (status != STATUS_SUCCESS)
+			debug3("LsaFreeMemory failed with ntstatus: %d \n", status);
+	}
 	return ret;
 }
 
@@ -547,31 +551,35 @@ HANDLE generate_sshd_virtual_token()
 	/* Initialize SIDs */
 	/* domain SID - S-1-5-111 */
 	if (!(AllocateAndInitializeSid(&nt_authority,
-	    1,
-	    111,
-	    0,
-	    0,
-	    0,
-	    0,
-	    0,
-	    0,
-	    0,
-	    &sid_domain)))
+		1,
+		111,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		&sid_domain))) {
+		debug3("AllocateAndInitializeSid failed with domain SID");
 		goto cleanup;
+	}
 
 	/* group SID - S-1-5-111-0 */
 	if (!(AllocateAndInitializeSid(&nt_authority,
-	    2,
-	    111,
-	    0,
-	    0,
-	    0,
-	    0,
-	    0,
-	    0,
-	    0,
-	    &sid_group)))
+		2,
+		111,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		&sid_group))) {
+		debug3("AllocateAndInitializeSid failed with group SID");
 		goto cleanup;
+	}
 
 	/* 
 	 * account SID 
@@ -580,29 +588,38 @@ HANDLE generate_sshd_virtual_token()
 	 * Ex account SID - S-1-5-111-3847866527-469524349-687026318-516638107-1125189541-123
 	 */
 	if (!(AllocateAndInitializeSid(&nt_authority,
-	    7,
-	    111,
-	    3847866527,
-	    469524349,
-	    687026318,
-	    516638107,
-	    1125189541,
-	    GetCurrentProcessId(),
-	    0,
-	    &sid_user)))
+		7,
+		111,
+		3847866527,
+		469524349,
+		687026318,
+		516638107,
+		1125189541,
+		GetCurrentProcessId(),
+		0,
+		&sid_user))) {
+		debug3("AllocateAndInitializeSid failed with account SID");
 		goto cleanup;
+	}
+		
 
 	/* Map the domain SID */
-	if (AddSidMappingToLsa(&domain, NULL, sid_domain) != 0)
+	if (AddSidMappingToLsa(&domain, NULL, sid_domain) != 0) {
+		debug3("AddSidMappingToLsa failed to map the domain Sid");
 		goto cleanup;
+	}		
 
 	/* Map the group SID */
-	if (AddSidMappingToLsa(&domain, &group, sid_group) != 0)
+	if (AddSidMappingToLsa(&domain, &group, sid_group) != 0) {
+		debug3("AddSidMappingToLsa failed to map the group Sid");
 		goto cleanup;
+	}
 
 	/* Map the user SID */
-	if (AddSidMappingToLsa(&domain, &account, sid_user) != 0)
+	if (AddSidMappingToLsa(&domain, &account, sid_user) != 0) {
+		debug3("AddSidMappingToLsa failed to map the user Sid");
 		goto cleanup;
+	}
 
 	/* assign service logon privilege to virtual account */
 	{
@@ -614,12 +631,12 @@ HANDLE generate_sshd_virtual_token()
 		if ((lsa_ret = LsaOpenPolicy(NULL, &ObjectAttributes,
 		    POLICY_CREATE_ACCOUNT | POLICY_LOOKUP_NAMES, 
 		    &lsa_policy )) != STATUS_SUCCESS) {
-			error("%s: unable to open policy handle, error: %d", __FUNCTION__, LsaNtStatusToWinError(lsa_ret));
+			error("%s: unable to open policy handle, error: %d", __FUNCTION__, pRtlNtStatusToDosError(lsa_ret));
 			goto cleanup;
 		}
 		InitUnicodeString(&svcLogonRight, L"SeServiceLogonRight");
 		if ((lsa_ret = LsaAddAccountRights(lsa_policy, sid_user, &svcLogonRight, 1)) != STATUS_SUCCESS) {
-			error("%s: unable to assign SE_SERVICE_LOGON_NAME privilege, error: %d", __FUNCTION__, LsaNtStatusToWinError(lsa_ret));
+			error("%s: unable to assign SE_SERVICE_LOGON_NAME privilege, error: %d", __FUNCTION__, pRtlNtStatusToDosError(lsa_ret));
 			goto cleanup;
 		}
 	}
