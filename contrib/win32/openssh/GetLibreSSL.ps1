9856691c722a3c16b45fb11a7de1a66063af2e0a
Yanbing Wang <yawang@microsoft.com>
2018-09-17 15:49:22 -0700

    1. rename script file 2. add override switch

--- contrib/win32/openssh/GetLibreSSL.ps1
+++ contrib/win32/openssh/GetLibreSSL.ps1
@@ -1,32 +1,41 @@
 ﻿param (
         [string]$sourceUrl = "https://github.com/PowerShell/libressl/releases/latest/",
         [string]$zipDir,
-        [string]$destPath)
+        [string]$destDir,
+        [switch]$override
+        )
 
 If ($PSVersiontable.PSVersion.Major -le 2) {$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path}
-#workaround that $PSScriptRoot is not support on ps version 2
+# Workaround that $PSScriptRoot is not support on ps version 2
 if([string]::IsNullOrEmpty($zipDir))
 {
     $zipDir = $PSScriptRoot
 }
-if([string]::IsNullOrEmpty($destPath))
+
+if([string]::IsNullOrEmpty($destDir))
 {
-    $destPath = $PSScriptRoot
+    $destDir = $PSScriptRoot
 }
-if (Test-Path (Join-Path $destPath "LibreSSL"))
+
+if($override)
+{
+    Remove-Item (join-path $destDir "LibreSSL") -Recurse -Force -ErrorAction SilentlyContinue
+}
+elseif (Test-Path (Join-Path $destDir "LibreSSL"))
 {
     return
 }
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor `
                                                   [Net.SecurityProtocolType]::Tls11 -bor `
                                                   [Net.SecurityProtocolType]::Tls
     
     $request = [System.Net.WebRequest]::Create($sourceUrl)
     $request.AllowAutoRedirect = $false
-    $request.Timeout = 30000; #30 sec
-    $response=$request.GetResponse()
-    $release_url=$([String]$response.GetResponseHeader("Location")).Replace('tag','download') + '/LibreSSL.zip' 
-    $zip_path=Join-Path $zipDir "libressl.zip"   
+    $request.Timeout = 60000; #30 sec
+    $response = $request.GetResponse()
+    $release_url = $([String]$response.GetResponseHeader("Location")).Replace('tag','download') + '/LibreSSL.zip' 
+    $zip_path = Join-Path $zipDir "libressl.zip"
 
     #download libressl latest release binaries
     Remove-Item $zip_path -Force -ErrorAction SilentlyContinue
@@ -35,10 +44,12 @@ if (Test-Path (Join-Path $destPath "LibreSSL"))
     {
         throw "failed to download ssl zip file"
     }
+    
     #copy libressl
-    Expand-Archive -Path $zip_path -DestinationPath $destpath -Force -ErrorAction SilentlyContinue -ErrorVariable e
+    Expand-Archive -Path $zip_path -DestinationPath $destDir -Force -ErrorAction SilentlyContinue -ErrorVariable e
     if($e -ne $null)
     {
         throw "Error when expand zip file"
     }
+    
     Remove-Item $zip_path -Force -ErrorAction SilentlyContinue
