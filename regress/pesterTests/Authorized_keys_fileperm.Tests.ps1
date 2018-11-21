4cd5273e63798a693ca30a73027efb0978778d45
Yanbing Wang <yawang@microsoft.com>
2018-11-20 17:00:59 -0800

    Add log

--- regress/pesterTests/Authorized_keys_fileperm.Tests.ps1
+++ regress/pesterTests/Authorized_keys_fileperm.Tests.ps1
@@ -5,7 +5,7 @@ $suite = "authorized_keys_fileperm"
 $testDir = "$env:temp\$suite"
 . $PSScriptRoot\common.ps1 -suite $suite -TestDir $testDir
 Import-Module "$Script:SSHBinaryPath\OpenSSHUtils" -force
-Describe "Tests for authorized_keys file permission" -Tags "Scenario" {
+Describe "Tests for authorized_keys file permission" -Tags "CI" {
     BeforeAll {
         $sshLogName = "test.txt"
         $sshdLogName = "sshdlog.txt"
@@ -13,7 +13,7 @@ Describe "Tests for authorized_keys file permission" -Tags "Scenario" {
         $server = "localhost"
 
         #$PwdUser = $OpenSSHTestInfo["PasswdUser"]
-        #$ssouserProfile = $OpenSSHTestInfo["SSOUserProfile"]        
+        #$ssouserProfile = $OpenSSHTestInfo["SSOUserProfile"]
         Remove-Item -Path (Join-Path $testDir "*$sshLogName") -Force -ErrorAction SilentlyContinue
     }
 
@@ -24,11 +24,10 @@ Describe "Tests for authorized_keys file permission" -Tags "Scenario" {
             $systemSid = Get-UserSID -WellKnownSidType ([System.Security.Principal.WellKnownSidType]::LocalSystemSid)
             $adminsSid = Get-UserSID -WellKnownSidType ([System.Security.Principal.WellKnownSidType]::BuiltinAdministratorsSid)
             $currentUserSid = Get-UserSID -User "$($env:USERDOMAIN)\$($env:USERNAME)"
-            
+
             $ssh_config_file = "$testDir\ssh_config"
-        
             #other default vars: -TargetName "test_target" -user_key_type "ed25519" -user_key_file "$testDir\user_key_$user_key_type" -known_host_file "$testDir\known_hosts"
-            Set-TestCommons -port $port -Server $server -ssh_config_file $ssh_config_file
+            Set-TestCommons -port $port -Server $server -ssh_config_file $ssh_config_file -ExtraArglist "-E $logfile"
 
             $authorized_keys = $Script:Authorized_keys_file
                         
@@ -48,39 +47,40 @@ Describe "Tests for authorized_keys file permission" -Tags "Scenario" {
         }
 
         It "$tC.$tI-authorized_keys-positive(authorized_keys is owned by local system)"{
-			Write-Host "In $tC.$tI"
+            Write-Host "In $tC.$tI"
             #setup to have system as owner and grant it full control
             Repair-FilePermission -Filepath $authorized_keys -Owner $systemSid -FullAccessNeeded  $adminsSid,$systemSid,$currentUserSid -confirm:$false
             $o = ssh  -F $ssh_config_file test_target echo 1234
             $o | Should Be "1234"
-			Write-Host "finish $tC.$tI"
+            Write-Host "finish $tC.$tI"
+            Write-Host Get-Process -name sshd
         }
 
         It "$tC.$tI-authorized_keys-positive(authorized_keys is owned by admins group and pwd does not have explict ACE)" {
-			Write-Host "In $tC.$tI"
+            Write-Host "In $tC.$tI"
             #setup to have admin group as owner and grant it full control
             Repair-FilePermission -Filepath $authorized_keys -Owner $adminsSid -FullAccessNeeded $adminsSid,$systemSid -confirm:$false
             $o = ssh  -F $ssh_config_file test_target echo 1234
             $o | Should Be "1234"
-			Write-Host "finish $tC.$tI"
+            Write-Host "finish $tC.$tI"
         }
 
         It "$tC.$tI-authorized_keys-positive(authorized_keys is owned by admins group and pwd have explict ACE)" {
-			Write-Host "In $tC.$tI"
+            Write-Host "In $tC.$tI"
             #setup to have admin group as owner and grant it full control
             Repair-FilePermission -Filepath $authorized_keys -Owner $adminsSid -FullAccessNeeded $adminsSid,$systemSid,$currentUserSid -confirm:$false
             $o = ssh  -F $ssh_config_file test_target echo 1234
             $o | Should Be "1234"
-			Write-Host "finish $tC.$tI"
+            Write-Host "finish $tC.$tI"
         }
 
         It "$tC.$tI-authorized_keys-positive(pwd user is the owner)" {
-			Write-Host "In $tC.$tI"
+            Write-Host "In $tC.$tI"
             #setup to have ssouser as owner and grant current user read and write, admins group, and local system full control
             Repair-FilePermission -Filepath $authorized_keys -Owners $currentUserSid -FullAccessNeeded  $adminsSid,$systemSid,$currentUserSid -confirm:$false
             $o = ssh -F $ssh_config_file test_target echo 1234
             $o | Should Be "1234"
-			Write-Host "finish $tC.$tI"
+            Write-Host "finish $tC.$tI"
         }
 
         <#It "$tC.$tI-authorized_keys-negative(authorized_keys is owned by other admin user)"{
@@ -93,7 +93,7 @@ Describe "Tests for authorized_keys file permission" -Tags "Scenario" {
             $LASTEXITCODE | Should Not Be 0
             Stop-SSHDTestDaemon                  
             $sshlog | Should -FileContentMatch "Permission denied"
-            $sshdlog | Should -FileContentMatch "Authentication refused."            
+            $sshdlog | Should -FileContentMatch "Authentication refused."
         }
 
         It "$tC.$tI-authorized_keys-negative(other account can access private key file)"{
@@ -107,7 +107,7 @@ Describe "Tests for authorized_keys file permission" -Tags "Scenario" {
             #Run
             Start-SSHDTestDaemon -workDir $opensshbinpath -Arguments "-d -p $port -o `"AuthorizedKeysFile .testssh/authorized_keys`" -E $sshdlog"
             ssh -p $port -E $sshlog -o "UserKnownHostsFile $testknownhosts" $ssouser@$server echo 1234
-            $LASTEXITCODE | Should Not Be 0            
+            $LASTEXITCODE | Should Not Be 0
             Stop-SSHDTestDaemon
             $sshlog | Should Contain "Permission denied"
             $sshdlog | Should Contain "Authentication refused."
